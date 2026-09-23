25a429605d26cc849786fa60af7cfca18593d610
cyanmint <20249732+cyanmint@users.noreply.github.com>
2026-09-23 06:36:11 +0000

    fix: restore two-finger cursor movement

--- Blink/WebKit/WKWebView.swift
+++ Blink/WebKit/WKWebView.swift
@@ -318,16 +318,14 @@ class UIScrollViewWithoutHitTest: UIScrollView {
       _scrollView.panGestureRecognizer.dropTouches()
       recognizer.view?.superview?.dropSuperViewTouches()
       _scrollView.isScrollEnabled = false
-      _reportedY = point.y
+      _scrollPointTrackpad = point
     case .changed:
-      let deltaY = point.y - _reportedY
-      guard abs(deltaY) >= 5 else { return }
-      _reportedY = point.y
-      _pinchRecognizer.dropTouches()
+      guard _scrollPointTrackpad != nil else { return }
       _2fTapRecognizer.dropTouches()
-      _wkWebView?.evaluateJavaScript("term_reportWheelEvent(\"wheel\", \(point.x), \(point.y), 0, \(deltaY));", completionHandler: nil)
+      _wkWebView?.evaluateJavaScript("term_reportMouseEvent(\"mousemove\", \(point.x), \(point.y), 0);", completionHandler: nil)
     case .ended, .cancelled, .failed:
       _scrollView.isScrollEnabled = true
+      _scrollPointTrackpad = nil
     default:
       break
     }
